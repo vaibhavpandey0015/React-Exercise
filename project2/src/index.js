79d7bfec0ec928f994d6b3ff39240945b5976003
Vaibhav Pandey <vaibhavpandey0015@gmail.com>
2021-01-07 01:44:45 +0530

    day2 done

--- project2/src/index.js
+++ project2/src/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import ReactDom from 'react-dom'     
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
-function Greeting(){
-  return <h3>This is vaibhav pandey and this my component</h3>;
-  
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => (
+  <img
+    src="https://images-eu.ssl-images-amazon.com/images/I/91sJmZFOIQL._AC_UL200_SR200,200_.jpg"
+    alt="image"
+  />
+);
 
-ReactDom.render(<Greeting/>,document.getElementById('root'))
+const Title = () => <h1>Word Power Made Easy Word Power Made Easy</h1>;
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0,75rem", marginTop: "0.25rem" }}>
+    Norman Lewis
+  </h4>
+);
+
+ReactDom.render(<BookList />, document.getElementById("root"));
