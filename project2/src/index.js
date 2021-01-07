1a291db65d7cdf0d6a1bfd51a05d579694280e58
Vaibhav Pandey <vaibhavpandey0015@gmail.com>
2021-01-08 01:27:33 +0530

    project2 -day3

--- project2/src/index.js
+++ project2/src/index.js
@@ -2,36 +2,50 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
+const books = [
+  {
+    id: 1,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/91sJmZFOIQL._AC_UL200_SR200,200_.jpg",
+    title: "Word Power Made Easy",
+    author: " Norman Lewis",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/91DBkHW1%2BtL._AC_UL200_SR200,200_.jpg",
+    title: "Bhagavad Gita: Yatharoop (Hindi)",
+    author: " A.C. Bhaktivendanta Swami Prabhupada",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/8170Bj%2BbSOL._AC_UL200_SR200,200_.jpg",
+    title: "The Power of Your Subconscious Mind",
+    author: "Joseph Murphy",
+  },
+];
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book, index) => {
+        // return <Book img={img} title={title} author={author}></Book>;
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
-const Book = () => {
+
+const Book = (props) => {
+  console.log(props);
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt="face" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
-const Image = () => (
-  <img
-    src="https://images-eu.ssl-images-amazon.com/images/I/91sJmZFOIQL._AC_UL200_SR200,200_.jpg"
-    alt="image"
-  />
-);
-
-const Title = () => <h1>Word Power Made Easy Word Power Made Easy</h1>;
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0,75rem", marginTop: "0.25rem" }}>
-    Norman Lewis
-  </h4>
-);
-
 ReactDom.render(<BookList />, document.getElementById("root"));
